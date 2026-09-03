2e4927cbb44262066c29e4c00e8b79530a1212d9
NagasriHarini <hariniraj8899@gmail.com>
2026-09-03 14:48:51 +0530

    Add 80 problem

---
+++ 0080_Remove_Duplicates_from_Sorted_Array II/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        i=2
+        for j in range(2,len(nums)):
+            if nums[i-2]!=nums[j]:
+                nums[i]=nums[j]
+                i+=1
+        return i
