1382b031796e1cef4214104b1a853bd95bbf4478
NagasriHarini <hariniraj8899@gmail.com>
2026-09-23 23:35:08 +0530

    added_125_question

---
+++ 0125_Is_Palindrome/Solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        i = 0
+        n = len(s) - 1
+
+        while i < n:
+
+            while i < n and not s[i].isalnum():
+                i += 1
+
+            while i < n and not s[n].isalnum():
+                n -= 1
+
+            if s[i].lower() != s[n].lower():
+                return False
+
+            i += 1
+            n -= 1
+
+        return True
